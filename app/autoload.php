0d9c00973e4ad74bb2fc681b9126ecee52e7c405
rondgrt <rondgrt@Linux-mint-16>
2014-04-19 10:11:14 +0200

    After removing PDF libraries and functions

--- app/autoload.php
+++ app/autoload.php
@@ -12,11 +12,12 @@ $loader = require __DIR__.'/../vendor/autoload.php';
 //    'Fpdf_' => __DIR__.'/../vendor/fpdf/lib',
 //));
 
-$loader->add('Fpdf_', __DIR__.'/../vendor/fpdf/lib');
-set_include_path(__DIR__.'/../vendor/fpdf/lib'.PATH_SEPARATOR.get_include_path());
-
-$loader->add('Tcpdf_', __DIR__.'/../vendor/tcpdf/lib');
-set_include_path(__DIR__.'/../vendor/tcpdf/lib'.PATH_SEPARATOR.get_include_path());
+//Use if PDF libraries needed
+//$loader->add('Fpdf_', __DIR__.'/../vendor/fpdf/lib');
+//set_include_path(__DIR__.'/../vendor/fpdf/lib'.PATH_SEPARATOR.get_include_path());
+//
+//$loader->add('Tcpdf_', __DIR__.'/../vendor/tcpdf/lib');
+//set_include_path(__DIR__.'/../vendor/tcpdf/lib'.PATH_SEPARATOR.get_include_path());
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
